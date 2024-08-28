1b18e0af6c55d1dd4be7a2a7531cd8e70257c42f
Wilson Andres Bachiller Ortiz <wabox324@gmail.com>
2024-08-27 23:04:55 -0500

    se realiza interfaz de chat

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface ChatResponse {
+  // Define la estructura de la respuesta esperada aquí
+  id: number;
+  question: string;
+  response: string;
+  // Otros campos según tu API
+}
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
 
+export class ChatComponent {
+  classId: string | null = null;
+  chatData:ChatResponse[] = [];
+  activeInput = true;
+
+  textChat = "";
+  constructor(
+    private chatService: ChatService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.classId = params.get('id');
+      this.chatService.getChatData(this.classId).subscribe(data => {
+        this.chatData = data;
+        console.log(this.chatData);
+      }, error => {
+        console.error('Error fetching data', error);
+      });
+    });
+  }
+
+  sendMessage() {
+    console.log("hola")
+    this.chatService.postChatData(this.classId, this.textChat).subscribe((data: ChatResponse)  => {
+      this.chatData.push(data)
+      this.textChat = "";
+    }, error => {
+        console.error('Error fetching data', error);
+    });
+  }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface ChatResponse {
+  // Define la estructura de la respuesta esperada aquí
+  id: number;
+  question: string;
+  response: string;
+  // Otros campos según tu API
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChatService {
 
-  constructor() { }
+
+
+export class ChatService {
+    
+  private apiUrl = 'http://127.0.0.1:8086/chats';
+
+  constructor(private http: HttpClient) { }
+
+
+  getChatData(id: string | null ): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  postChatData(id: string | null, question: string ): Observable<ChatResponse> {
+    return this.http.post<ChatResponse>(`${this.apiUrl}/${id}`, { question: question});
+  }
 }
